c70889c72b8f7326b15fa020959db496524d766d
Muhammad Mudassar Nazar <mudassarn@gmail.com>
2013-09-06 12:13:50 -0400

    Added Title Tests and passes them

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,17 +6,30 @@ describe "Static Pages" do
       visit '/static_pages/home'
       expect(page).to have_content ('Sample App')
     end
+    it "should have title 'Ruby on Rails Tutorial Sample App | Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+
   end
   describe "Help Page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content ("Help")
     end
+    it "should have title 'Ruby on Rails Tutorial Sample App | Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
   end
   describe "About Page" do
     it "should have content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content ("About Us")
+    end
+    it "should have title 'Ruby on Rails Tutorial Sample App | About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
     end
   end
 end
